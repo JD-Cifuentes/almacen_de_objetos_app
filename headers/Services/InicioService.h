41c584a79c244504141654aedc57eac6407e2a90
Felipe <felipe.calan@correounivale.edu.co>
2024-04-06 18:20:11 -0500

    creacion deploy local

---
+++ headers/Services/InicioService.h
@@ -0,0 +1,23 @@
+//
+// Created by felip on 29/03/2024.
+//
+
+#ifndef PROYECTO_INICIOSERVICE_H
+#define PROYECTO_INICIOSERVICE_H
+#include <iostream>
+#include "../../headers/Vista/MenuInicio.h"
+#include "../../headers/Vista/MenuFactura.h"
+
+class InicioService{
+public:
+    void Irmenuproducto();
+    void Irmenucliente();
+    void Irmenufactura();
+};
+
+
+#endif //PROYECTO_INICIOSERVICE_H
+
+
+
+
